2b4f3014c8d052a6e6fcf48a8650816d2f1dec05
Matheus Pereira <152235011+Matheuszovisk25@users.noreply.github.com>
2025-04-18 15:29:11 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,105 @@
+from fastapi import FastAPI, Query
+from embrapa import Embrapa
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/" , tags=["Rota_teste"])
+def read_root():
+    return {"mensagem": "API Embrapa"}
+
+@app.get("/producao", tags=["Producao"])
+def get_producao(ano: int = Query(2023, description="Ano desejado para os dados")):
+    s = Embrapa()
+    dados = s.producao(ano)
+    return {f"Producao Vinhos Embrapa - {ano}": dados}
+
+@app.get('/comercializacao', tags=["Comercializacao"])
+def get_comercializacao(ano: int = Query(2021, description="Ano desejado para os dados1: ")):
+    s = Embrapa()
+    dados = s.comercializacao(ano)
+    return {f"Comercializacao de vinhos - {ano}": dados}
+
+@app.get('/processamento/viniferas', tags=["Processamento"])
+def get_processamento_viniferas():
+    s = Embrapa()
+    dados = s.processamento_viniferas()
+    return {"Processamento de viniferas": dados}
+
+@app.get('/processamento/americanas', tags=["Processamento"])
+def get_processamento_americanas():
+    s = Embrapa()
+    dados = s.processamento_americanas_e_hibridas()
+    return {"Processamento americanas e hibridas": dados}
+
+@app.get('/processamento/uvas_de_mesa', tags=["Processamento"])
+def get_processamento_uvas_de_mesa():
+    s = Embrapa()
+    dados = s.processamento_uvas_de_mesa()
+    return {"Processamento uvas de mesa": dados}
+
+@app.get('/importacao/vinhos_de_mesa', tags=["Importação"])
+def importacao_vinhos_de_mesa():
+    s = Embrapa()
+    dados = s.importacao_vinhos_de_mesa()
+    return {"Importacao vinhos de mesa": dados}
+
+@app.get('/importacao/espumantes', tags=["Importação"])
+def importacao_espumantes():
+    s = Embrapa()
+    dados = s.importacao_espumantes()
+    return {"Importacao vinhos de mesa": dados}
+
+@app.get('/importacao/uvas_frescas', tags=["Importação"])
+def importacao_uvas_frescas():
+    s = Embrapa()
+    dados = s.importacao_uvas_frescas()
+    return {"Importacao uvas frescas": dados}
+
+@app.get('/importacao/uvas_passas', tags=["Importação"])
+def importacao_uvas_passas():
+    s = Embrapa()
+    dados = s.importacao_uvas_passas()
+    return {"Importacao uvas_passas": dados}
+
+@app.get('/importacao/suco_de_uvas', tags=["Importação"])
+def importacao_suco_de_uva():
+    s = Embrapa()
+    dados = s.importacao_suco_de_uva()
+    return {"Importacao suco de uva": dados}
+
+@app.get('/exportacao/vinho_de_mesa', tags=["Exportação"])
+def exportacao_vinho_de_mesa():
+    s = Embrapa()
+    dados = s.exportacao_suco_de_uva()
+    return {"Exportacao suco de uva": dados}
+
+@app.get('/exportacao/espumantes', tags=["Exportação"])
+def exportacao_espumantes():
+    s = Embrapa()
+    dados = s.exportacao_espumantes()
+    return {"Exportação espumantes:": dados}
+
+@app.get('/exportacao/uvas_frescas', tags=["Exportação"])
+def exportacao_uvas_frescas():
+    s = Embrapa()
+    dados = s.exportacao_uvas_frescas()
+    return {"Exportação uvas frescas": dados}
+
+@app.get('/exportacao/suco_de_uva', tags=["Exportação"])
+def exportacao_suco_de_uva():
+    s = Embrapa()
+    dados = s.exportacao_suco_de_uva()
+    return {"Exportação suco de uva": dados}
+
+
+
+
